25d59f7b18f57a6cbd8a874d99506bae76640455
DanielBash <ddbash@icloud.com>
2026-02-23 17:57:02 +0300

    3.3 Solved

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+professions = 'Водопроводчик Ниндзя Тусовщица Анимешник Переводчик Стилист Продакт-менеджер Профессор-математики'.split()
 
 
 @app.route('/<string:title>')
@@ -15,6 +16,13 @@ def training(prof):
     if 'инженер' in prof or 'строитель' in prof:
         training_target = 'Инженерные тренажеры'
     return render_template('training.html', training_target=training_target)
+
+
+@app.route('/list_prof/<string:display>')
+def list_prof(display):
+    if display not in ['ul', 'ol']:
+        return f'Неверный параметр display: {display}.'
+    return render_template('list_prof.html', display=display, professions=professions)
 
 
 if __name__ == '__main__':
